6ea5a9f6362afd1eb47ecf55c894d72758d10235
Jsjsjdju <118336176+Jsjsjdju@users.noreply.github.com>
2022-11-16 18:29:17 +0530

    Update config.js

--- config.js
+++ config.js
@@ -23,7 +23,7 @@ global.sessionName = "session"
 global.prefa = ['.'] // Prefix ---  change it to anythong you want except @//
 
 
-global.location = "West Bengal, India"
+global.location = "ASSAM, India"
 global.reactmoji = "❤️"
 global.themeemoji = "💖"
 global.vidmenu = { url: 'https://c.tenor.com/YGuLegQWubwAAAPo/miku-nakano-gotoubun-no-hanayome.mp4' }
